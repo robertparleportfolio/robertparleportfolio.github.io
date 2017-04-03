ec2f2d46a64bd248e3b9a2443568466b5414e7db
robertjparle <newrobertparle@hotmail.co.uk>
2017-04-03 14:29:28 +0100

    Adding files to the repo for hosting

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+//Javascript that allows me to print my email address without it getting scraped by bots
+var name = "newrobertparle";
+var domain = "hotmail.co.uk";
+document.getElementById('emailaddress').innerHTML = name + "@" + domain;
